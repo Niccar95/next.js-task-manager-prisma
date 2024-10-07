1cf14a22a00d5ac493ba3c92e0b4a426e057297c
Niccar95 <niccar.95@outlook.com>
2024-10-07 18:54:05 +0200

    can now toggle todos

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -1,36 +1,25 @@
 "use client";
-import React, { useState } from 'react'
-
 
 interface TodoItemProps {
-    id: string;
-    title: string;
-    complete: boolean;
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
 }
 
-const TodoItem = ({id, complete, title }: TodoItemProps) => {
-    const [isComplete, setIsComplete] = useState<boolean>(complete);
-
-   const handleComplete = () => {
-
-    const completeStatus = !isComplete;
-
-    setIsComplete(completeStatus);
-
-    }
-
-    console.log(isComplete);
-
-    
-
+const TodoItem = ({ id, complete, title, toggleTodo }: TodoItemProps) => {
   return (
     <li>
-        <p>{title}</p>
-       <input id={id} type='checkbox' onChange={handleComplete}></input> 
-       <label htmlFor={id}></label>
-      
+      <p>{title}</p>
+      <input
+        id={id}
+        type="checkbox"
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+        defaultChecked={complete}
+      ></input>
+      <label htmlFor={id}></label>
     </li>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
--- app/page.tsx
+++ app/page.tsx
@@ -7,34 +7,40 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
-async function deleteCompletedTodos() {
-const result = await prisma.todo.deleteMany({
-    where: {
-      complete: true, // Delete all todos that are marked as complete
-    },
-   
-  });
-  return result.count; 
+// async function deleteCompletedTodos() {
+//   const result = await prisma.todo.deleteMany({
+//     where: {
+//       complete: true,
+//     },
+//   });
+//   return result.count;
+// }
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
 }
 
-
 export default async function Page() {
-  
-  const deleted = await deleteCompletedTodos();  
+  //const deleted = await deleteCompletedTodos();
   const todos = await getTodos();
 
-  console.log(todos)
-  console.log(deleted);
-
-
-
+  console.log(todos);
+  //console.log(deleted);
 
   return (
     <>
       <h1>To Do</h1>
       <ul>
-        {todos.map(todo => (
-          <TodoItem key={todo.id} id={todo.id} title={todo.title} complete={todo.complete} ></TodoItem>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            complete={todo.complete}
+            toggleTodo={toggleTodo}
+          ></TodoItem>
         ))}
       </ul>
     </>
